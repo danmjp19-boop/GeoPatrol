131717c398790be2b521e49f8a7c9409461c0417
danmjp19-boop <danmjp19@gmail.com>
2026-06-22 21:36:54 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 USUARIOS_FILE = os.path.join(BASE_DIR, "..", "database", "usuarios.json")
